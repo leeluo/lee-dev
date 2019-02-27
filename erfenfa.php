e12ca0c95244efbd86c0fe71a0fffe37933bd8ee
luoxiyang <269736851@qq.com>
2019-02-27 15:33:16 +0800

    二分法完善

--- erfenfa.php
+++ erfenfa.php
@@ -4,13 +4,21 @@
  ************/
 
 //二分查找法仅适用于有序数组，算法时间复杂度为O(logn)
+//类比事件：100以内猜数字游戏，
+//假设目标数字为79
+//第一次猜50，小了，区间定位为[51-100];
+//第二次猜75，小了，区间定位为[76-100];
+//第三次猜85，大了，区间定位为[76-84];
+//第四次猜80，大了，区间定位为[76-79];
+//第五次猜78，小了，区间定位为[79];
+
 function searchByMid($list, $value)
 {
     $start = 0;
     $end = count($list)-1;
     echo '目标数组：'.json_encode($list).' 查找数据：'.$value.'<br>';
     $i = 0;
-    while ($start <= $end) {
+    while ($start <= $end) {  //确保最后一次循环只定位到一个元素
         $i++;
         $mid = floor(($start+$end)/2);
         $guess = $list[$mid];
@@ -20,11 +28,11 @@ function searchByMid($list, $value)
             return '该元素存在于数组中第'.$mid.'位'; //定位到元素后返回元素位置并结束循环（使用return结束循环，或者break跳出循环）
             //break;
         }
-        if ($guess < $value) {
+        if ($guess < $value) {  //中间值小于目标数据时，区间缩小为[mid+1, end]
             $start = $mid+1;
             echo '第 '.$i.' 次循环：start：'.$start.' end:'.$end.' mid:'.$mid.'<br>';
         }
-        if ($guess > $value) {
+        if ($guess > $value) { //中间值大于目标数据时，区间缩小为[start, mid-1]
             $end = $mid-1;
             echo '第 '.$i.' 次循环：start：'.$start.' end:'.$end.' mid:'.$mid.'<br>';
         }
