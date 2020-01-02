dfbc08241ebb284590a1a72027ea414b4dd4ee8b
luoxiyang <269736851@qq.com>
2020-01-02 10:45:22 +0800

    链表

---
+++ linkedlist/singLinkedList.php
@@ -0,0 +1,20 @@
+<?php
+
+/*单链表*/
+class singLinkedList
+{
+    //头结点
+    public $head;
+
+    //链表长度
+    public $length;
+
+    public function __construct()
+    {
+
+    }
+
+}
+
+
+
---
+++ linkedlist/singLinkedListNode.php
@@ -0,0 +1,21 @@
+<?php
+
+/*单链表结点*/
+class singLinkedListNode
+{
+    //链表结点数据块
+    public $data;
+
+    //链表结点指针
+    public $next;
+
+    public function __construct()
+    {
+
+    }
+
+
+}
+
+
+
